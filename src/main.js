609e56c84d028ac1621fed76ce96bc01c327e27a
Ashleigh Maund <ashleigh.maund@bcsg.com>
2016-11-18 10:33:30 +0000

    updating css and js files

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
-console.log('i\'m doing things');
-console.log('i am a monkeyyyy');
+var activePageAndTab = {
+    'activeNavTab': null,
+    'activePageView': null
+};
+
+function viewPage(idOfPageToView, idOfNavTabClicked) {
+    // view page and make tab active
+    addClassById(idOfNavTabClicked, 'activeNavTab', 'active-nav')
+    addClassById(idOfPageToView, 'activePageView', 'active-page-view');
+};
+
+var addClassById = function (idOfElementClicked, lastActiveElement, classToAdd) {
+    if (activePageAndTab[lastActiveElement] != null) {
+        document.getElementById(activePageAndTab[lastActiveElement]).classList.remove(classToAdd);
+    }
+    document.getElementById(idOfElementClicked).classList.add(classToAdd);
+    activePageAndTab[lastActiveElement] = idOfElementClicked;
+};
