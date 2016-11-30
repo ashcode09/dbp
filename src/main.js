2b54f18b926ba9ae4c50458e5c8fd03d50d66265
Ashleigh Maund <ashleigh.maund@bcsg.com>
2016-11-30 18:30:56 +0000

    amending navigation padding, styling on product page tabs, adding search box so that value on first page is inserted into second page search box

--- src/main.js
+++ src/main.js
@@ -1,3 +1,35 @@
+var searchBoxOpen = false;
+
+var openSearchInputBox = function (objectClicked) {
+    var siblingsOfObjectClicked = Array.prototype.slice.call(objectClicked.parentNode.childNodes);
+    if (!searchBoxOpen) {
+        for (var i=0; i<siblingsOfObjectClicked.length; i++) {
+            var classListOfSibling = [];
+            if (siblingsOfObjectClicked[i].classList != undefined) {
+                classListOfSibling = Array.prototype.slice.call(siblingsOfObjectClicked[i].classList);
+            }
+            if (classListOfSibling != undefined && classListOfSibling.indexOf('searchboxcontainer') >= 0) {
+                document.getElementById(siblingsOfObjectClicked[i].id).classList.add('open-search-box');
+            }
+            if (document.getElementById('searchText').value.length > 0) {
+                document.getElementById('searchTextInsert').value = document.getElementById('searchText').value;
+            }
+        }
+
+        searchBoxOpen = true;
+    } else {
+        for (var i=0; i<siblingsOfObjectClicked.length; i++) {
+            var classListOfSibling = [];
+            if (siblingsOfObjectClicked[i].classList != undefined) {
+                classListOfSibling = Array.prototype.slice.call(siblingsOfObjectClicked[i].classList);
+            }
+            if (classListOfSibling != undefined && classListOfSibling.indexOf('open-search-box') >= 0) {
+                document.getElementById(siblingsOfObjectClicked[i].id).classList.remove('open-search-box');
+            }
+        }
+        searchBoxOpen = false;
+    }
+};
 var activePageAndTabAndSideBarBtn = {
     'activeNavTab': null,
     'activeSideBarBtn': null,
